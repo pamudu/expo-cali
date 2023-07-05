2be487ed108babc01c62ff6eb8f75e98a6d453bb
pamudu <pamuduprabathiya28@gmail.com>
2023-07-05 17:09:41 +0530

    add domain package

---
+++ src/main/java/com/example/ec/demo/domain/Difficulty.java
@@ -0,0 +1,4 @@
+package com.example.ec.demo.domain;
+
+public enum Difficulty {
+}
---
+++ src/main/java/com/example/ec/demo/domain/Tour.java
@@ -0,0 +1,57 @@
+package com.example.ec.demo.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CollectionId;
+
+@Entity
+public class Tour {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Column(length = 2000)
+    private String blurb;
+
+    @Column
+    private Integer price;
+
+    @Column
+    private String duration;
+
+    @Column(length = 2000)
+    private String bullets;
+
+    @Column
+    private String keywords;
+
+    @ManyToOne
+    private TourPackage tourPackage;
+
+    @Column
+    @Enumerated
+    private Difficulty difficulty;
+
+    @Column
+    @Enumerated
+    private Region region;
+
+    public Tour(Integer id, String title, String description, String blurb, Integer price, String duration, String bullets, String keywords, TourPackage tourPackage, Difficulty difficulty, Region region) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.blurb = blurb;
+        this.price = price;
+        this.duration = duration;
+        this.bullets = bullets;
+        this.keywords = keywords;
+        this.tourPackage = tourPackage;
+        this.difficulty = difficulty;
+        this.region = region;
+    }
+}
