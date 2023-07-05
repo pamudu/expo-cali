686c8b8da92060a96fbc22c33dc59de2155bc8a8
pamudu <pamuduprabathiya28@gmail.com>
2023-07-05 11:37:50 +0530

    Initialize the project

---
+++ src/main/java/com/example/ec/demo/ExpocaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpocaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpocaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ec/demo/ExpocaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpocaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
